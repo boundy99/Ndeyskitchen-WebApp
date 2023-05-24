e15296b4591da1451b264bbef6fc21ac2ec02668
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-05-24 03:39:37 -0400

    Feat: Login Page done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,25 @@
 export default function Login() {
   return (
-    <div>
-      <h1> Hello </h1>
+    <div className="login">
+      <form>
+        <h1 className="form-header">Login</h1>
+        <label className="form-label" htmlFor="email">
+          Email
+        </label>
+
+        <input className="input-box" type="email" id="email" />
+
+        <label className="form-label" htmlFor="password">
+          Password
+        </label>
+
+        <input className="input-box" type="password" id="password" />
+
+        <button className="form-btn">Login</button>
+        <p className="form-link">
+          Don't have an account? <a href="/">Signup here</a>
+        </p>
+      </form>
     </div>
   );
 }
