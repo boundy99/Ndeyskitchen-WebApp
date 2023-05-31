5b8aad23ea65fe23d550c4675cf3d5f08d801bd0
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-05-30 23:46:11 -0400

    Feat: Home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import Profile from '../pages/Profile';
 import Signup from '../pages/Signup';
+import Home from '../pages/Home';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <BrowserRouter>
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Profile />} />
+            <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
           </Routes>
